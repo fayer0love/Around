5a9e4fb5df60ad831192b3eb1ce19d075aa414ed
Shiyang Li <sli99@hawk.iit.edu>
2018-05-17 22:48:14 -0500

    change ES URL

--- service/main.go
+++ service/main.go
@@ -38,7 +38,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.229.43.99:9200"
+	ES_URL = "http://35.231.145.96:9200"
 	// Needs to update this bucket based on your gcs bucket name.
 	BUCKET_NAME = "fayer0love-around"
 
